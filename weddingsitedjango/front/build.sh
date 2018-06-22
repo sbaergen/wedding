bbbb9db62973b65a75de546c4791acc5006367f8
Sean Baergen <sbaergen@ualberta.ca>
2018-06-22 11:07:17 -0400

    Served on django

---
+++ weddingsitedjango/front/build.sh
@@ -0,0 +1,7 @@
+ng build
+mv dist/front/main.js ../wedding/static/
+mv dist/front/polyfills.js ../wedding/static/
+mv dist/front/runtime.js ../wedding/static/
+mv dist/front/styles.js ../wedding/static/
+mv dist/front/vendor.js ../wedding/static/
+
--- weddingsitedjango/weddingsitedjango/urls.py
+++ weddingsitedjango/weddingsitedjango/urls.py
@@ -19,10 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('wedding/', include('wedding.urls')),
-    path('', include('wedding.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns.append(path('', include('wedding.urls')))
