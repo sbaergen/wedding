3785bb59e584c4b4452abeb27b5bea3d623657ed
Sean Baergen <sbaergen@ualberta.ca>
2018-07-29 17:45:05 -0400

    Work on RSVP

--- weddingsitedjango/front/src/app/app.module.ts
+++ weddingsitedjango/front/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { AccomodationsComponent } from './accomodations/accomodations.component';
@@ -9,8 +11,11 @@ import { EventComponent } from './event/event.component';
 import { RegistryComponent } from './registry/registry.component';
 import { PhotosComponent } from './photos/photos.component';
 import { RsvpComponent } from './rsvp/rsvp.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule, MatCardModule, MatInputModule, MatButtonModule} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -26,7 +31,16 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule.forRoot(),
-    HttpModule
+    HttpModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- weddingsitedjango/front/src/app/rsvp/rsvp.component.ts
+++ weddingsitedjango/front/src/app/rsvp/rsvp.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/Rx';
+import 'rxjs/add/operator/toPromise';
 @Component({
   selector: 'app-rsvp',
   templateUrl: './rsvp.component.html',
   styleUrls: ['./rsvp.component.css']
 })
 export class RsvpComponent implements OnInit {
-
-  constructor() { }
+  correct : boolean = false;
+  url : string='http://localhost:8000/wedding/rsvp/all/';
+  constructor(private http : Http) { }
 
   ngOnInit() {
+  }
+
+  checkCode(code): void {
+    if (code.value == "ST2019"){
+        this.correct = true;
+    } else {
+        this.correct = false;
+    }
+  }
+
+  getNames(first, last): void {
+    this.http.get(this.url).toPromise().then((res)=>{console.log(res.json());
+    })
   }
 
 }
